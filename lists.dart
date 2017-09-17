af87f95b28b8eadb50055ea190511855d397e9f9
Tim Sneath <tim@sneath.org>
2017-09-17 16:33:05 -0700

    add a cascade

--- lists.dart
+++ lists.dart
@@ -9,6 +9,13 @@ main() {
   
   // why does this not print [1, 2, 3]?
   print('${ints[3]}');
-  
+
+  // TODO: map a list to double its value
+
+  // cascade can be used like with / end with in VB
+  fibonacci
+  ..add(55 + 89)
+  ..add(89 + (55 + 89));
+
   print('\$fibonacci: $fibonacci');
 }
