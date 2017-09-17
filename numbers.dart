6d2e3badac98b9378c33a96e29ff08694e46ac54
Tim Sneath <tim@sneath.org>
2017-09-16 17:01:10 -0700

    Initial commit with numbers.dart

---
+++ numbers.dart
@@ -0,0 +1,25 @@
+printNumber(num aNumber)
+{
+  print('The number is $aNumber.');
+  for (; aNumber < 1000; aNumber++);
+  print('After incrementing, the number is $aNumber.');
+}
+
+main() {
+  var number = 42;
+  printNumber(number);
+  print('Numbers are passed by value. The number is still $number.');
+
+
+  // more precision than the Dart double type can handle
+  var pi = 3.1415926535897932384626433832;
+  print('π ≈ $pi');
+  print('π is even more roughly ${pi.toStringAsPrecision(3)}');
+
+  DateTime myBirth = new DateTime.utc(1973, DateTime.AUGUST, 26, );
+  print('I was born on ${myBirth.toString()}');
+  print('Was I born on a Sunday? ${myBirth.weekday == 7 ? 'Yes' : 'No'}');
+  print('The type of \$myBirth is ${myBirth.runtimeType}');
+  DateTime myBirth2 = new DateTime.utc(1973, DateTime.AUGUST, 26);
+  print('\$myBirth == \$myBirth2? ${myBirth == myBirth2}');
+}
