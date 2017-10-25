3e06e52251e3f20634299b7aa0dd86d1cc7cc49a
Tim Sneath <tim@sneath.org>
2017-10-24 17:42:37 -0700

    add a sublist scenario

--- strings.dart
+++ strings.dart
@@ -1,15 +1,17 @@
 String reverse(String str) {
   String str2 = '';
-  
+
   for (var i = str.length - 1; i >= 0; i--) {
     str2 += str[i];
   }
   return str2;
 }
 
+String createPhoneNumber(List numbers) =>
+    '(${numbers.sublist(0,3).join()}) ${numbers.sublist(3,6).join()}-${numbers.sublist(6,10).join()}';
 
-
-void main() 
-{
+main() {
   print(reverse('Hello world'));
+  print(createPhoneNumber(
+      [1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // => returns "(123) 456-7890"
 }
